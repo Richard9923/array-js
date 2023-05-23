1ed8d4d52f4dd65ef10aabbed934138dca696eb3
Raffael2323 <118739446+Raffael2323@users.noreply.github.com>
2023-05-23 18:08:35 -0300

    aprendendo

---
+++ dividindo-array.js
@@ -0,0 +1,6 @@
+const alunos = ['joao', 'juliana', 'andressa', 'ana', 'caio','andre', 'ronaldo'];
+
+const sala1 = alunos.slice(0, alunos.length / 2);
+const sala2 = alunos.slice(alunos.length / 2);
+console.log(sala1);
+console.log(sala2);
---
+++ justando-salas.js
@@ -0,0 +1,6 @@
+const salaJS = ['Evaldo', 'Camis'];
+const salaPython = ['Ju', 'Leo'];
+
+const salasJuntas = salaJS.concat(salaPython);
+
+console.log(salasJuntas);
